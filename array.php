c57084fc1a9ba0e91aade704c6d80d7bacaccde3
Saadakhtaraptech <saadakhtaraptech@gmail.com>
2026-02-24 07:41:57 +0500

    Add files via upload

---
+++ array.php
@@ -0,0 +1,16 @@
+<?php
+
+// array
+
+$saad = array("Saad", "Ali", "Ahmed", "Hassan");
+echo "<br>";
+// echo $saad[1] . "<br>";
+// echo $saad[2] . "<br>";
+// echo $saad[3] . "<br>";
+
+
+foreach($saad as $name){
+    echo $name . "<br>";
+}
+
+?>
---
+++ loop.php
@@ -0,0 +1,21 @@
+<?php
+// for loop 
+
+for($a = 1; $a <= 10; $a++){
+    echo "2 x " . $a . " = " . (2 * $a) . "<br>";
+}
+
+for($a = 10; $a >= 1; $a--){
+    echo "2 x " . $a . " = " . (2 * $a) . "<br>";
+}
+
+
+// while loop
+
+$a = 11;
+while($a <= 16){
+    echo 'This is while loop ' . $a . "<br>";
+    $a++;
+}
+
+?>
